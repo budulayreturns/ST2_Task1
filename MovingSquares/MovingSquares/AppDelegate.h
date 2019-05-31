1cfc03d5969c58f5eaa1bdcd1ad31e196dc62f5d
Dzmitry Shabailau <budulayreturns@gmail.com>
2019-05-31 20:49:19 +0300

    first commit

---
+++ MovingSquares/MovingSquares/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovingSquares
+//
+//  Created by Дмитрий on 5/31/19.
+//  Copyright © 2019 Dzmitry Shabailau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovingSquares/MovingSquares/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MovingSquares
+//
+//  Created by Дмитрий on 5/31/19.
+//  Copyright © 2019 Dzmitry Shabailau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
